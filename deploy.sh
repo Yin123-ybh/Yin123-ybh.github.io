eb646c6c97f0a14ab6dd19b3dace743a18002c9b
Yin123-ybh <3388949919@qq.com>
2025-09-13 14:31:09 +0800

    Update site 2025-09-13 14:31:09

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-cd ~/my-blog || exit 1
-hugo || exit 1
-cd public || exit 1
+# 设置错误时退出
+set -e
 
+# 进入博客目录
+cd /Users/yin/my-blog
+
+# 构建 Hugo 站点
+echo "正在构建 Hugo 站点..."
+hugo --minify
+
+# 复制所有构建文件到根目录
+echo "正在复制文件到根目录..."
+cp -r public/* .
+
+# 添加所有更改
+echo "正在添加文件到 Git..."
 git add .
+
+# 提交更改
+echo "正在提交更改..."
 git commit -m "Update site $(date '+%Y-%m-%d %H:%M:%S')"
-git push -u origin main -f
+
+# 推送到 master 分支
+echo "正在推送到 GitHub..."
+git push origin master
 
 echo "Hugo 博客已成功部署！"
-
+echo "请访问：https://yin123-ybh.github.io/"
